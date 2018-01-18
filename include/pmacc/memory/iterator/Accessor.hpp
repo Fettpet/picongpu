d8c1fb54ecbd5f12f5570da89227aa30c9a902bf
Sebastian Hahn <seb.hahn1991@gmail.com>
2018-01-19 00:22:29 +0100

    Change the format of accessor such that the coding style is respected

--- include/pmacc/memory/iterator/Accessor.hpp
+++ include/pmacc/memory/iterator/Accessor.hpp
@@ -46,11 +46,13 @@ class UndefinedAhead
     typedef char one;
     typedef long two;
 
-    template <typename C> static one test( decltype(&C::UNDEFINED) ) ;
+    template <typename C> static one test( decltype(&C::UNDEFINED) );
     template <typename C> static two test(...);    
 
 public:
-    enum { value = sizeof(test<T>(0)) == sizeof(char) };
+    enum 
+    { 
+        value = sizeof(test<T>(0)) == sizeof(char) };
 }; // class UndefinedAhead
 
 struct UndefinedType;
@@ -63,58 +65,84 @@ struct UndefinedType;
  * 
  * @brief The Accesor is used to describe the current position of the iterator. 
  * The Accessor use several functions: 
- * 1. TComponent& get(TContaier*, TIndex&): returns the current value at the 
+ * 1. T_Component& get(TContaier*, T_Index&): returns the current value at the 
  * iterator position
- * 2. bool equal(TContainer*, TIndex&, TContainer*, TIndex&): returns true,
+ * 2. bool equal(T_Container*, T_Index&, T_Container*, T_Index&): returns true,
  * if the iterator is at the same position
- * 3. bool ahead(TContainer*, TIndex&, TContainer*, TIndex&): returns true,
+ * 3. bool ahead(T_Container*, T_Index&, T_Container*, T_Index&): returns true,
  * if the first iterator is ahead the second one. 
- * 4. bool behind(TContainer* , TIndex&, TContainer*, TIndex&): returns true,
+ * 4. bool behind(T_Container* , T_Index&, T_Container*, T_Index&): returns true,
  * if the first iterator is behind the second one.
  * The functions ahead and behind are only avail, if the iterator is random 
  * accessable. 
  * To use the default Accessor you need to spezify one trait for each function:
- * 1. get: pmacc::traits::accessor::Get<TContainer, TComponent, TIndex>
- * 2. ahead: pmacc::traits::accessor::Ahead<TContainer, TComponent, TIndex>
- * 3. equal: pmacc::traits::accessor::Equal<TContainer, TComponent, TIndex>
- * 4. behind: pmacc::traits::accessor::Behind<TContainer, TComponent, TIndex>
- * @tparam TContainer The container over which you like to iterate. 
- * @tparam TComponent The type of the container component. 
- * @tparam TIndex Type of the index to get access to the value of the iterator 
+ * 1. get: pmacc::traits::accessor::Get<T_Container, T_Component, T_Index>
+ * 2. ahead: pmacc::traits::accessor::Ahead<T_Container, T_Component, T_Index>
+ * 3. equal: pmacc::traits::accessor::Equal<T_Container, T_Component, T_Index>
+ * 4. behind: pmacc::traits::accessor::Behind<T_Container, T_Component, T_Index>
+ * @tparam T_Container The container over which you like to iterate. 
+ * @tparam T_Component The type of the container component. 
+ * @tparam T_Index Type of the index to get access to the value of the iterator 
  * position.
- * @tparam TContainerCategory Type of default access parameter
- * @tparam TGet Trait to define, how to get the first element 
- * @tparam TEqual Trait to define, when two iterators are at the same position
- * @tparam TAhead Trait to define, when the first iterator is ahead the second 
+ * @tparam T_ContainerCategory Type of default access parameter
+ * @tparam T_Get Trait to define, how to get the first element 
+ * @tparam T_Equal Trait to define, when two iterators are at the same position
+ * @tparam T_Ahead Trait to define, when the first iterator is ahead the second 
  * one. Only needed if the iterator is random accessable.
- * @tparam TBehind Trait to define, when the first iterator is behind the second
+ * @tparam T_Behind Trait to define, when the first iterator is behind the second
  * one. Only needed if the iteartor is random accessable.
  * @tparam isRandomAccessable true, if the container is random accessable, false
  * otherwise
  */
 template<
-    typename TContainer,
-    typename TComponent,
-    typename TIndex,
-    typename TContainerCategory,
-    typename TGet = pmacc::traits::accessor::Get<TContainer, TComponent, TIndex, TContainerCategory>,
-    typename TAhead = pmacc::traits::accessor::Ahead<TContainer, TComponent, TIndex, TContainerCategory>,
-    typename TEqual = pmacc::traits::accessor::Equal<TContainer, TComponent, TIndex, TContainerCategory>,
-    typename TBehind = pmacc::traits::accessor::Behind<TContainer, TComponent, TIndex, TContainerCategory>,
-    bool isRandomAccessable = not details::UndefinedAhead<TAhead>::value >
+    typename T_Container,
+    typename T_Component,
+    typename T_Index,
+    typename T_ContainerCategory,
+    typename T_Get = pmacc::traits::accessor::Get<
+        T_Container, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Ahead = pmacc::traits::accessor::Ahead<
+        T_Container, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Equal = pmacc::traits::accessor::Equal<
+        T_Container, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Behind = pmacc::traits::accessor::Behind<
+        T_Container, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    bool isRandomAccessable = not details::UndefinedAhead<T_Ahead>::value >
 struct Accessor
 {
-    typedef TContainer                                              ContainerType;
-    typedef ContainerType*                                          ContainerPtr;
-    typedef TComponent                                              ComponentType;
-    typedef ComponentType*                                          ComponentPtr;
-    typedef ComponentType&                                          ComponentRef;
-    typedef TContainerCategory                                      ContainerCategory;
-    typedef TIndex                                                  IndexType;
+    typedef T_Container                                     ContainerType;
+    typedef ContainerType*                                  ContainerPtr;
+    typedef T_Component                                     ComponentType;
+    typedef ComponentType*                                  ComponentPtr;
+    typedef ComponentType&                                  ComponentRef;
+    typedef T_ContainerCategory                             ContainerCategory;
+    typedef T_Index                                         IndexType;
     
     HDINLINE Accessor() = default;
-    HDINLINE Accessor(Accessor const &) = default;
-    HDINLINE Accessor(Accessor &&) = default;
+    
+    HDINLINE Accessor(
+        Accessor const &
+    ) = default;
+    
+    HDINLINE Accessor(
+        Accessor &&
+    ) = default;
     
     /**
      * @brief this function dereference a container and a index to get the 
@@ -124,16 +152,23 @@ struct Accessor
      * @return component at the idx position
      */
     HDINLINE 
-    ComponentRef
-    get(ContainerPtr containerPtr,
-        IndexType & idx)
+    auto
+    get(
+        ContainerPtr containerPtr,
+        IndexType & idx
+    )
+    -> ComponentRef
     {
         assert(containerPtr != nullptr);
-        return _get(containerPtr, idx);
+        
+        return _get(
+            containerPtr, 
+            idx
+        );
     }
     
     
-        /**
+    /**
      * @brief this function compares two iterator positions. 
      * @param containerPtr1 pointer to the container of the first iterator
      * @param idx1 current position of the first iterator 
@@ -142,15 +177,24 @@ struct Accessor
      * @return true if both iterators are at the same position, false otherwise
      */
     HDINLINE
-    bool
-    equal(ContainerPtr const containerPtr1,
-          IndexType const & index1,
-          ContainerPtr const containerPtr2,
-          IndexType const & index2)
+    auto
+    equal(
+        ContainerPtr const containerPtr1,
+        IndexType const & index1,
+        ContainerPtr const containerPtr2,
+        IndexType const & index2
+    )
+    -> bool
     {
         assert(containerPtr1 != nullptr);
         assert(containerPtr2 != nullptr);
-        return _equal(containerPtr1, index1, containerPtr2, index2);
+        
+        return _equal(
+            containerPtr1, 
+            index1, 
+            containerPtr2, 
+            index2
+        );
     }
     
     /**
@@ -164,15 +208,22 @@ struct Accessor
      */
     template<bool T = isRandomAccessable>
     HDINLINE 
-    typename std::enable_if<T == true, bool>::type
+    auto
     greater(ContainerPtr const containerPtr1,
           IndexType const & index1,
           ContainerPtr const containerPtr2,
           IndexType const & index2)
+    -> typename std::enable_if<T == true, bool>::type
     {
         assert(containerPtr1 != nullptr);
         assert(containerPtr2 != nullptr);
-        return _ahead(containerPtr1, index1, containerPtr2, index2);
+        
+        return _ahead(
+            containerPtr1, 
+            index1, 
+            containerPtr2, 
+            index2
+        );
     }
     
     /**
@@ -186,23 +237,28 @@ struct Accessor
      */
     template<bool T = isRandomAccessable>
     HDINLINE 
-    typename std::enable_if<T == true, bool>::type
+    auto
     lesser(ContainerPtr const containerPtr1,
           IndexType const & index1,
           ContainerPtr const containerPtr2,
           IndexType const & index2)
+    -> typename std::enable_if<T == true, bool>::type
     {
         assert(containerPtr1 != nullptr);
         assert(containerPtr2 != nullptr);
-        return _behind(containerPtr1, index1, containerPtr2, index2);
+        
+        return _behind(
+            containerPtr1, 
+            index1, 
+            containerPtr2,
+            index2
+        );
     }
     
-    
-    
-     TGet _get;
-     TAhead _ahead;
-     TEqual _equal;
-     TBehind _behind;
+    T_Get _get;
+    T_Ahead _ahead;
+    T_Equal _equal;
+    T_Behind _behind;
 } ;
 
 /**
@@ -217,13 +273,18 @@ struct Accessor<
     pmacc::details::UndefinedType,
     pmacc::details::UndefinedType,
     pmacc::details::UndefinedType,
-    pmacc::details::UndefinedType>
+    pmacc::details::UndefinedType
+>
 {
     typedef details::UndefinedType ContainerType;
     
     HDINLINE Accessor() = default;
-    HDINLINE Accessor(Accessor const &) = default;
-    HDINLINE Accessor(Accessor &&) = default;
+    HDINLINE Accessor(
+        Accessor const &
+    ) = default;
+    HDINLINE Accessor(
+        Accessor &&
+    ) = default;
 } ;
 
 namespace details 
@@ -241,39 +302,61 @@ namespace details
  * 5. Ahead
  * 6. Equal
  * 7. Behind
- * @param TAccessor The accessor prescription. It is only virtual and not needed
- * @tparam TContainer. Type of the container.
+ * @param T_Accessor The accessor prescription. It is only virtual and not needed
+ * @tparam T_Container. Type of the container.
  * @return An accessor with the functionallity given by the pmacc::traits.
  */
 template<
-    typename TContainer,
-    typename TContainerNoRef = typename std::decay<TContainer>::type,
-    typename TIndex = typename pmacc::traits::IndexType<TContainerNoRef>::type,
-    typename TContainerCategory = typename pmacc::traits::ContainerCategory<typename std::decay<TContainer>::type>::type,
-    typename TComponent = typename pmacc::traits::ComponentType<TContainerNoRef>::type,
-    typename TGet = pmacc::traits::accessor::Get<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TAhead = pmacc::traits::accessor::Ahead<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TEqual = pmacc::traits::accessor::Equal<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TBehind = pmacc::traits::accessor::Behind<TContainerNoRef, TComponent, TIndex, TContainerCategory>, 
-    typename TAccessor>
+    typename T_Container,
+    typename T_ContainerNoRef = typename std::decay<T_Container>::type,
+    typename T_Index = typename pmacc::traits::IndexType<T_ContainerNoRef>::type,
+    typename T_ContainerCategory = typename pmacc::traits::ContainerCategory<typename std::decay<T_Container>::type>::type,
+    typename T_Component = typename pmacc::traits::ComponentType<T_ContainerNoRef>::type,
+    typename T_Get = pmacc::traits::accessor::Get<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Ahead = pmacc::traits::accessor::Ahead<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Equal = pmacc::traits::accessor::Equal<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Behind = pmacc::traits::accessor::Behind<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >, 
+    typename T_Accessor>
 HDINLINE
 auto 
-makeAccessor(TAccessor&&)
+makeAccessor(
+    T_Accessor&&
+)
 -> pmacc::Accessor<
-    TContainerNoRef,
-    TComponent,
-    TIndex,
-    TContainerCategory,
-    TGet,
-    TAhead,
-    TEqual,
-    TBehind>
+    T_ContainerNoRef,
+    T_Component,
+    T_Index,
+    T_ContainerCategory,
+    T_Get,
+    T_Ahead,
+    T_Equal,
+    T_Behind>
 {
     typedef pmacc::Accessor<
-        TContainerNoRef,
-        TComponent,
-        TIndex,
-        TContainerCategory> AccessorType;
+        T_ContainerNoRef,
+        T_Component,
+        T_Index,
+        T_ContainerCategory> AccessorType;
     auto && accessor = AccessorType();
     return accessor;
 }
@@ -288,41 +371,62 @@ makeAccessor(TAccessor&&)
  * 5. Ahead
  * 6. Equal
  * 7. Behind
- * @tparam TContainer. Type of the container.
+ * @tparam T_Container. Type of the container.
  * @return An accessor with the functionallity given by the pmacc::traits.
  */
 template<
-    typename TContainer,
-    typename TContainerNoRef = typename std::decay<TContainer>::type,
-    typename TIndex = typename pmacc::traits::IndexType<TContainerNoRef>::type,
-    typename TContainerCategory = typename pmacc::traits::ContainerCategory<typename std::decay<TContainer>::type>::type,
-    typename TComponent = typename pmacc::traits::ComponentType<TContainerNoRef>::type,
-    typename TGet = pmacc::traits::accessor::Get<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TAhead = pmacc::traits::accessor::Ahead<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TEqual = pmacc::traits::accessor::Equal<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TBehind = pmacc::traits::accessor::Behind<TContainerNoRef, TComponent, TIndex, TContainerCategory> >
+    typename T_Container,
+    typename T_ContainerNoRef = typename std::decay<T_Container>::type,
+    typename T_Index = typename pmacc::traits::IndexType<T_ContainerNoRef>::type,
+    typename T_ContainerCategory = typename pmacc::traits::ContainerCategory<typename std::decay<T_Container>::type>::type,
+    typename T_Component = typename pmacc::traits::ComponentType<T_ContainerNoRef>::type,
+    typename T_Get = pmacc::traits::accessor::Get<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Ahead = pmacc::traits::accessor::Ahead<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Equal = pmacc::traits::accessor::Equal<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Behind = pmacc::traits::accessor::Behind<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >
+>
 auto 
 HDINLINE
 makeAccessor()
 -> pmacc::Accessor<
-    TContainerNoRef,
-    TComponent,
-    TIndex,
-    TContainerCategory,
-    TGet,
-    TAhead,
-    TEqual,
-    TBehind>
+    T_ContainerNoRef,
+    T_Component,
+    T_Index,
+    T_ContainerCategory,
+    T_Get,
+    T_Ahead,
+    T_Equal,
+    T_Behind>
 {
     typedef pmacc::Accessor<
-        TContainerNoRef,
-        TComponent,
-        TIndex,
-        TContainerCategory,
-        TGet,
-        TAhead,
-        TEqual,
-        TBehind>                                          ResultType;
+        T_ContainerNoRef,
+        T_Component,
+        T_Index,
+        T_ContainerCategory,
+        T_Get,
+        T_Ahead,
+        T_Equal,
+        T_Behind>                                          ResultType;
         
     return ResultType();
 }
@@ -340,42 +444,64 @@ makeAccessor()
  * 5. Ahead
  * 6. Equal
  * 7. Behind
- * @param TContainer. The container
+ * @param T_Container. The container
  * @return An accessor with the functionallity given by the pmacc::traits.
  */
 template<
-    typename TContainer,
-    typename TContainerNoRef = typename std::decay<TContainer>::type,
-    typename TIndex = typename pmacc::traits::IndexType<TContainerNoRef>::type,
-    typename TContainerCategory = typename pmacc::traits::ContainerCategory<typename std::decay<TContainer>::type>::type,
-    typename TComponent = typename pmacc::traits::ComponentType<TContainerNoRef>::type,
-    typename TGet = pmacc::traits::accessor::Get<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TAhead = pmacc::traits::accessor::Ahead<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TEqual = pmacc::traits::accessor::Equal<TContainerNoRef, TComponent, TIndex, TContainerCategory>,
-    typename TBehind = pmacc::traits::accessor::Behind<TContainerNoRef, TComponent, TIndex, TContainerCategory> >
-    
+    typename T_Container,
+    typename T_ContainerNoRef = typename std::decay<T_Container>::type,
+    typename T_Index = typename pmacc::traits::IndexType<T_ContainerNoRef>::type,
+    typename T_ContainerCategory = typename pmacc::traits::ContainerCategory<typename std::decay<T_Container>::type>::type,
+    typename T_Component = typename pmacc::traits::ComponentType<T_ContainerNoRef>::type,
+    typename T_Get = pmacc::traits::accessor::Get<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Ahead = pmacc::traits::accessor::Ahead<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Equal = pmacc::traits::accessor::Equal<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >,
+    typename T_Behind = pmacc::traits::accessor::Behind<
+        T_ContainerNoRef, 
+        T_Component, 
+        T_Index, 
+        T_ContainerCategory
+    >
+>
 auto 
 HDINLINE
-makeAccessor(TContainer&&)
+makeAccessor(
+    T_Container&&
+)
 -> pmacc::Accessor<
-    TContainerNoRef,
-    TComponent,
-    TIndex,
-    TContainerCategory,
-    TGet,
-    TAhead,
-    TEqual,
-    TBehind>
+    T_ContainerNoRef,
+    T_Component,
+    T_Index,
+    T_ContainerCategory,
+    T_Get,
+    T_Ahead,
+    T_Equal,
+    T_Behind>
 {
     typedef pmacc::Accessor<
-        TContainerNoRef,
-        TComponent,
-        TIndex,
-        TContainerCategory,
-        TGet,
-        TAhead,
-        TEqual,
-        TBehind>                                         ResultType;
+        T_ContainerNoRef,
+        T_Component,
+        T_Index,
+        T_ContainerCategory,
+        T_Get,
+        T_Ahead,
+        T_Equal,
+        T_Behind>                                         ResultType;
         
     return ResultType();
 }
@@ -419,14 +545,18 @@ makeAccessor()
  * @return the accessor given by as parameter.
  */
 template<
-    typename TAccessor>
+    typename T_Accessor>
 HDINLINE
 auto
-makeAccessor(TAccessor && accessor)
+makeAccessor(
+    T_Accessor && accessor
+)
 ->
-decltype(pmacc::forward<TAccessor>(accessor))
+decltype(
+    pmacc::forward<T_Accessor>(accessor)
+)
 {
-    return pmacc::forward<TAccessor>(accessor);
+    return pmacc::forward<T_Accessor>(accessor);
 }
 
 }// namespace pmacc
